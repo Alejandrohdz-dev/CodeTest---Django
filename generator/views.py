0ed17c2697c0f29cc5f93fcea7b90cbe00844917
Alejandro <alejandrohdz.dev@gmail.com>
2020-12-19 17:05:03 -0400

    Add ListCreateAPIView for products

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,12 @@
+from django.db.models import query
+from rest_framework import generics, serializers
 from django.shortcuts import render
 
-# Create your views here.
+from .models import MasterProductsConfigurable
+from .serializers import ProductsSerializer
+
+
+class ProductsListCreateAPIView(generics.ListCreateAPIView):
+    """Provide a read-write view for products"""
+    queryset = MasterProductsConfigurable.objects.all()
+    serializer_class = ProductsSerializer
