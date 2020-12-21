e0e6450165e7a32759006e514d5204bcf8ef1fc0
Alejandro <alejandrohdz.dev@gmail.com>
2020-12-20 21:13:53 -0400

    Add 'cvs-file-generator' path

--- generator/urls.py
+++ generator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
-from .views import ProductsListCreateAPIView
+from .views import ProductsListCreateAPIView, CVSGenerator, cvs_file_generator
 
 urlpatterns = [
-    path("api/products/", ProductsListCreateAPIView.as_view(),name="products-list"),
+    path('', CVSGenerator.as_view(), name='home_page'),
+    path("api/products/", ProductsListCreateAPIView.as_view(),name="products_list"),
+    path('cvs-file-generator', cvs_file_generator, name='csv_file_generator'),
 ]
