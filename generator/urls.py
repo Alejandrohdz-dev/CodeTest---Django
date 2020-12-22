ead9e0fdeaff191d9bd0acc9de3cec54877b1978
Alejandro <alejandrohdz.dev@gmail.com>
2020-12-22 19:24:25 -0400

    Update the api url

--- generator/urls.py
+++ generator/urls.py
@@ -3,6 +3,6 @@ from .views import ProductsListCreateAPIView, CVSGenerator, cvs_file_generator
 
 urlpatterns = [
     path('', CVSGenerator.as_view(), name='home_page'),
-    path("api/products/", ProductsListCreateAPIView.as_view(),name="products_list"),
+    path('api/products/', ProductsListCreateAPIView.as_view(),name='products_list'),
     path('cvs-file-generator', cvs_file_generator, name='csv_file_generator'),
 ]
