17f897d3b306841258ca08b4917369e22ca573a3
Alejandro Hernandez <70658760+Alejandrohdz-dev@users.noreply.github.com>
2021-01-06 15:10:03 -0400

    Update urls.py

--- generator/urls.py
+++ generator/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
-from .views import ProductsListCreateAPIView, CVSGenerator, cvs_file_generator
+from .views import ProductsListCreateAPIView, CSVGenerator, csv_file_generator
 
 urlpatterns = [
-    path('', CVSGenerator.as_view(), name='home_page'),
+    path('', CSVGenerator.as_view(), name='home_page'),
     path('api/products/', ProductsListCreateAPIView.as_view(),name='products_list'),
-    path('cvs-file-generator', cvs_file_generator, name='csv_file_generator'),
+    path('csv-file-generator', csv_file_generator, name='csv_file_generator'),
 ]
